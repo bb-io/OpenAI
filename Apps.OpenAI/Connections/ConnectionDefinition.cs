de0367c5347e05c7e8e8b64b523f6c42d104be17
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-09-01 11:57:45 +0300

    throw exception if connection parameters are not specified

--- Apps.OpenAI/Connections/ConnectionDefinition.cs
+++ Apps.OpenAI/Connections/ConnectionDefinition.cs
@@ -1,4 +1,5 @@
-﻿using Blackbird.Applications.Sdk.Common.Authentication;
+﻿using System;
+using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,18 +26,19 @@ public class ConnectionDefinition : IConnectionDefinition
     public IEnumerable<AuthenticationCredentialsProvider> CreateAuthorizationCredentialsProviders(
         Dictionary<string, string> values)
     {
-        var organizationId = values.First(v => v.Key == "Organization ID");
-        yield return new AuthenticationCredentialsProvider(
-            AuthenticationCredentialsRequestLocation.None,
-            organizationId.Key,
-            organizationId.Value
-        );
-
-        var apiKey = values.First(v => v.Key == "API key");
-        yield return new AuthenticationCredentialsProvider(
-            AuthenticationCredentialsRequestLocation.None,
-            apiKey.Key,
-            apiKey.Value
-        );
+        try
+        {
+            var organizationId = values.First(v => v.Key == "Organization ID");
+            var apiKey = values.First(v => v.Key == "API key");
+            return new AuthenticationCredentialsProvider[]
+            {
+                new(AuthenticationCredentialsRequestLocation.None, organizationId.Key, organizationId.Value),
+                new(AuthenticationCredentialsRequestLocation.None, apiKey.Key, apiKey.Value)
+            };
+        }
+        catch (InvalidOperationException)
+        {
+            throw new Exception("Organization ID and/or API key are not specified in connection.");
+        }
     }
 }
