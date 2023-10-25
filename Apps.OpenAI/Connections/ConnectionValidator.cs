99d63503667d506956388f0b3f40bd2c71214301
mathijs-bb <mathijs.sonnemans@blackbird.io>
2023-10-25 15:41:47 +0200

    added connection validator

---
+++ Apps.OpenAI/Connections/ConnectionValidator.cs
@@ -0,0 +1,41 @@
+﻿using Apps.OpenAI.Extensions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Apps.OpenAI.Connections
+{
+    public class ConnectionValidator : IConnectionValidator
+    {
+        public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+            IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+        {
+            var client = authProviders.CreateOpenAiServiceSdk();
+
+            try
+            {
+                var result = await client.Models.ListModel();
+                result.ThrowOnError();
+
+                return new()
+                {
+                    IsValid = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new()
+                {
+                    IsValid = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
