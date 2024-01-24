d48183fc56faa224d77c73728a9c58daa80e4a6a
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-01-24 18:42:44 +0100

    added extract glossary

---
+++ Apps.OpenAI/Models/Requests/Chat/ExtractGlossaryRequest.cs
@@ -0,0 +1,27 @@
+﻿using Apps.OpenAI.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.OpenAI.Models.Requests.Chat
+{
+    public class ExtractGlossaryRequest
+    {
+        [Display("Language (ISO 639-1)")]
+        [DataSource(typeof(IsoLanguageDataSourceHandler))]
+        public string Language { get; set; }
+
+        [Display("Content")]
+        public string Content { get; set; }
+
+        [Display("Temperature")]
+        [DataSource(typeof(TemperatureDataSourceHandler))]
+        public float? Temperature { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
---
+++ Apps.OpenAI/Models/Responses/Chat/GlossaryResponse.cs
@@ -0,0 +1,14 @@
+﻿using Blackbird.Applications.Sdk.Common.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.OpenAI.Models.Responses.Chat
+{
+    public class GlossaryResponse
+    {
+        public FileReference Glossary { get; set; }
+    }
+}
