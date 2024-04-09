e41c443c89e1f188f1a539dcc7a42292e24c1b31
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-09 19:11:59 +0300

    Added glossary to xliff action

--- Apps.OpenAI/Actions/ChatActions.cs
+++ Apps.OpenAI/Actions/ChatActions.cs
@@ -536,7 +536,8 @@ public class ChatActions : BaseActions
          Display("Prompt",
              Description =
                  "Specify the instruction to be applied to each source tag within a translation unit. For example, 'Translate text'")] string? prompt,
-        [ActionParameter, Display("Bucket size", Description = "Specify the number of source texts to be translated at once. Default value: 15")] int? bucketSize = 15)
+        [ActionParameter, Display("Bucket size", Description = "Specify the number of source texts to be translated at once. Default value: 15")] int? bucketSize = 15,
+        [ActionParameter] GlossaryRequest glossary)
     {
         var xliffDocument = await LoadAndParseXliffDocument(input.File);
         if (xliffDocument.TranslationUnits.Count == 0)
@@ -547,8 +548,20 @@ public class ChatActions : BaseActions
         var model = modelIdentifier.ModelId ?? "gpt-4-turbo-preview";
         string systemPrompt = GetSystemPrompt(string.IsNullOrEmpty(prompt));
         var list = xliffDocument.TranslationUnits.Select(x => x.Source).ToList();
+        
+        string? glossaryPrompt = null;
+        if (glossary != null)
+        {
+            glossaryPrompt += "Enhance the target text by incorporating relevant terms from our glossary where applicable. " +
+                              "Ensure that the translation aligns with the glossary entries for the respective languages. " +
+                              "If a term has variations or synonyms, consider them and choose the most appropriate " +
+                              "translation to maintain consistency and precision. ";
 
-        var translatedTexts = await GetTranslations(prompt, xliffDocument, model, systemPrompt, list, bucketSize ?? 15);
+            var glossaryPromptPart = await GetGlossaryPromptPart(glossary.Glossary);
+            glossaryPrompt += glossaryPromptPart;
+        }
+
+        var translatedTexts = await GetTranslations(prompt, xliffDocument, model, systemPrompt, list, bucketSize ?? 15, glossaryPrompt);
         
         var updatedDocument = UpdateXliffDocumentWithTranslations(xliffDocument, translatedTexts);
         var fileReference = await UploadUpdatedDocument(updatedDocument, input.File);
@@ -638,7 +651,7 @@ public class ChatActions : BaseActions
             new XliffConfig { RemoveWhitespaces = true, CopyAttributes = true });
     }
 
-    private async Task<string[]> GetTranslations(string prompt, XliffDocument xliffDocument, string model, string systemPrompt, List<string> sourceTexts, int bucketSize)
+    private async Task<string[]> GetTranslations(string prompt, XliffDocument xliffDocument, string model, string systemPrompt, List<string> sourceTexts, int bucketSize, string? glossaryPrompt)
     {
         List<string> allTranslatedTexts = new List<string>(); 
 
@@ -655,6 +668,10 @@ public class ChatActions : BaseActions
             string json = JsonConvert.SerializeObject(bucketSourceTexts);
 
             var userPrompt = GetUserPrompt(prompt, xliffDocument, json);
+            if (!string.IsNullOrEmpty(glossaryPrompt))
+            {
+                userPrompt += glossaryPrompt;
+            }
 
             var request = new OpenAIRequest("/chat/completions", Method.Post, Creds);
             request.AddJsonBody(new
