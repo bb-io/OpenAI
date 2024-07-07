f6eb0cbf95e3253105d24684bcf83d2052d3aa8b
ce-nistal <cecilia.nistal@blackbird.io>
2024-07-07 19:03:01 -0300

    Changes to Post edit XLIFF action

--- Apps.OpenAI/Utils/Xliff/Extensions.cs
+++ Apps.OpenAI/Utils/Xliff/Extensions.cs
@@ -1,5 +1,12 @@
-﻿using System.IO;
+﻿using Blackbird.Applications.Sdk.Common.Files;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Xliff.Utils.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace Apps.OpenAI.Utils.Xliff;
@@ -21,4 +28,76 @@ public static class Extensions
 
         return memoryStream;
     }
+    public static ParsedXliff ParseXLIFF(Stream file)
+    {
+        var xliffDocument = XDocument.Load(file);
+        XNamespace defaultNs = xliffDocument.Root.GetDefaultNamespace();
+        var tus = new List<TranslationUnit>();
+        
+        foreach (var tu in (from tu in xliffDocument.Descendants(defaultNs + "trans-unit") select tu).ToList())
+        {
+            tus.Add(new TranslationUnit
+            {
+                Source = RemoveExtraNewLines(Regex.Replace(tu.Element(defaultNs + "source").ToString(), @"</?source(.*?)>", @"")),
+                Target = RemoveExtraNewLines(Regex.Replace(tu.Element(defaultNs + "target").ToString(), @"</?target(.*?)>", @"")),
+                Id = tu.Attribute("id").Value                
+            });
+        }        
+
+        return new ParsedXliff 
+        {
+            SourceLanguage = xliffDocument.Root?.Element(defaultNs + "file")?.Attribute("source-language")?.Value,
+            TargetLanguage = xliffDocument.Root?.Element(defaultNs + "file")?.Attribute("target-language")?.Value,
+            TranslationUnits = tus
+        };
+    }
+    public static string RemoveExtraNewLines(string originalString)
+    {
+        if (!string.IsNullOrWhiteSpace(originalString))
+        {
+            var to_modify = originalString;
+            to_modify = Regex.Replace(to_modify, @"\r\n(\s+)?", "", RegexOptions.Multiline);
+            return to_modify;
+        }
+        else
+        {
+            return string.Empty;
+        }
+    }
+
+    public static Stream UpdateOriginalFile(Stream fileStream, Dictionary<string, string> results)
+    {
+        string fileContent;
+        Encoding encoding;
+        fileStream.Position = 0;
+        using (StreamReader inFileStream = new StreamReader(fileStream))
+        {
+            encoding = inFileStream.CurrentEncoding;
+            fileContent = inFileStream.ReadToEnd();
+        }
+
+        var tus = Regex.Matches(fileContent, @"<trans-unit [\s\S]+?</trans-unit>").Select(x => x.Value);
+        foreach (var tu in tus) 
+        { 
+            var id = Regex.Match(tu, @"trans-unit id=""(.*?)""").Groups[1].Value;
+            if (results.ContainsKey(id)) 
+            {
+                var newtu = Regex.Replace(tu, "(<target(.*?)>)([\\s\\S]+?)(</target>)", "${1}" + results[id] +"${4}");
+                fileContent = Regex.Replace(fileContent, Regex.Escape(tu), newtu);
+
+            } 
+            else continue;
+        }
+        return new MemoryStream(encoding.GetBytes(fileContent));
+    }
+
+}
+
+public class ParsedXliff
+{
+    public string SourceLanguage { get; set; }
+    public string TargetLanguage { get; set; }
+
+    public List<TranslationUnit> TranslationUnits { get; set; }
+
 }
