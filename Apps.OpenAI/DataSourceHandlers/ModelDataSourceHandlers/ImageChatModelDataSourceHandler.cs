e9a786d80f626e7e37d073ed5614522342aed48a
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-09-02 11:56:42 +0200

    Added model identifier to get localizable content

---
+++ Apps.OpenAI/DataSourceHandlers/ModelDataSourceHandlers/ImageChatModelDataSourceHandler.cs
@@ -0,0 +1,22 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.OpenAI.DataSourceHandlers.ModelDataSourceHandlers
+{
+    public class ImageChatModelDataSourceHandler : IStaticDataSourceHandler
+    {
+        public Dictionary<string, string> GetData()
+        {
+            return new Dictionary<string, string> {
+                { "gpt-4o", "gpt-4o"},
+                { "gpt-4o-mini", "gpt-4o-mini" },
+                { "chatgpt-4o-latest", "chatgpt-4o-latest" },
+                { "gpt-4-turbo", "gpt-4-turbo" }
+            };
+        }
+    }
+}
