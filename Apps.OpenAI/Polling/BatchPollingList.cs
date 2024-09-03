1acd62e869712ecad9bf94f89224da2c4afca2b3
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-03 11:55:13 +0300

    Add batch polling finished polling event

---
+++ Apps.OpenAI/Polling/BatchPollingList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Apps.OpenAI.Actions.Base;
+using Apps.OpenAI.Api;
+using Apps.OpenAI.Models.Identifiers;
+using Apps.OpenAI.Models.Responses.Batch;
+using Apps.OpenAI.Polling.Models;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+using RestSharp;
+
+namespace Apps.OpenAI.Polling;
+
+[PollingEventList]
+public class BatchPollingList(InvocationContext invocationContext) : BaseActions(invocationContext, null!)
+{
+    [PollingEvent("On batch finished", "Triggered when a batch status is set to completed")]
+    public async Task<PollingEventResponse<DateMemory, BatchResponse>> OnBatchFinished(
+        PollingEventRequest<DateMemory> request,
+        [PollingEventParameter] BatchIdentifier identifier)
+    {
+        if (request.Memory is null)
+        {
+            return new()
+            {
+                FlyBird = false,
+                Memory = new()
+                {
+                    LastPollingTime = DateTime.UtcNow
+                }
+            };
+        }
+        
+        var getBatchRequest = new OpenAIRequest($"/batches/{identifier.BatchId}", Method.Get, Creds);
+        var batch = await Client.ExecuteWithErrorHandling<BatchResponse>(getBatchRequest);
+        return new()
+        {
+            FlyBird = batch.Status == "completed",
+            Result = batch,
+            Memory = new()
+            {
+                LastPollingTime = DateTime.UtcNow
+            }
+        };
+    }
+}
---
+++ Apps.OpenAI/Polling/Models/DateMemory.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Apps.OpenAI.Polling.Models;
+
+public class DateMemory
+{
+    public DateTime? LastPollingTime { get; set; }
+}
