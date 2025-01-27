9fc398288f509e0613dd952aa69e6417692b1dae
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2025-01-27 12:33:31 +0200

    Fix serialization issue with 'Process XLIFF action' and improve error handling for 'Post-edit XLIFF action'

--- Apps.OpenAI/Utils/TryCatchHelper.cs
+++ Apps.OpenAI/Utils/TryCatchHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Blackbird.Applications.Sdk.Common.Exceptions;
+using Newtonsoft.Json;
 
 namespace Apps.OpenAI.Utils;
 
@@ -9,6 +11,11 @@ public class TryCatchHelper
         try
         {
             action();
+        }
+        catch (JsonException ex)
+        {
+            throw new PluginApplicationException($"The JSON we received from Open AI is invalid (this could be due to hallucination or because the output is too long and got cut off). " +
+                                                 $"Please try adding a retry policy to this action and/or lower the 'Bucket size' to avoid returning too long of a response.", ex);
         }
         catch (Exception ex)
         {
