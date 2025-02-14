d01ed5141d7af5042e59ba0205fc491e86fa8ef2
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-14 14:48:32 +0200

    Updated data handler

--- Tests.OpenAI/Chat.cs
+++ Tests.OpenAI/Chat.cs
@@ -32,7 +32,7 @@ namespace Tests.OpenAI
 
         [TestMethod]
         public async Task PostEditXliffResponse()
-        {
+        {//chatgpt-4o-latest
             var actions = new ChatActions(InvocationContext, FileManager);
             var input1 = new TextChatModelIdentifier { ModelId= "o1" };
             var input2 = new PostEditXliffRequest {File = new Blackbird.Applications.Sdk.Common.Files.FileReference { Name= "test.xliff" } };
@@ -40,5 +40,19 @@ namespace Tests.OpenAI
             var input4 = new GlossaryRequest { };
             var result = await actions.PostEditXLIFF(input1, input2, input3, input4);
         }
+
+        [TestMethod]
+        public async Task ProcessXliffResponse()
+        {
+            var actions = new ChatActions(InvocationContext, FileManager);
+            var input1 = new TextChatModelIdentifier { ModelId = "o1" };
+            var input2 = new TranslateXliffRequest { File = new Blackbird.Applications.Sdk.Common.Files.FileReference { Name = "test.xliff" } };
+            string? input3 = null;
+            var input4 = new GlossaryRequest { };
+            var result = await actions.TranslateXliff(input1, input2, input3, input4);
+        }
     }
 }
+
+//models that work with JSON schema:
+//gpt-4o, gpt-4o-2024-11-20, gpt-4o-mini, gpt-4o-mini-2024-07-18, gpt-4o-2024-08-06(and latest versions),  
