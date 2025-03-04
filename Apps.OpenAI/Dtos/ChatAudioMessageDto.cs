d4dafd4872212727a46264e0ad5ffc904dfeefc7
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-03-04 20:29:10 +0100

    Add audio support, WIP

---
+++ Apps.OpenAI/Dtos/ChatAudioMessageDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Apps.OpenAI.Dtos;
+
+public record ChatAudioMessageDto(string Role, List<ChatAudioMessageContentDto> Content) : BaseChatMessageDto(Role);
+
+public abstract record ChatAudioMessageContentDto(string Type);
+
+public record ChatAudioMessageTextContentDto(string Type, string Text) : ChatAudioMessageContentDto(Type);
+
+public record ChatAudioMessageAudioContentDto(string Type, AudioUrlDto Audio_url) : ChatAudioMessageContentDto(Type);
+
+public record AudioUrlDto(string Url);
