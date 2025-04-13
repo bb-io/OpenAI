ef59e6fd548a0e5ed33a7353c49a38b51ebcd5f0
ce-nistal <cecilia.nistal@blackbird.io>
2025-04-13 13:27:34 -0300

    Added MQM Xliff actions

--- Apps.OpenAI/Actions/ChatActions.cs
+++ Apps.OpenAI/Actions/ChatActions.cs
@@ -879,8 +879,119 @@ public class ChatActions(InvocationContext invocationContext, IFileManagementCli
         };
     }
 
+    [Action("Get MQM report from XLIFF",
+       Description = "Perform an LQA Analysis of the translated XLIFF file. The result will be in the MQM framework form.")]
+    public async Task<ChatResponse> GetLqaAnalysisFromXliff([ActionParameter] TextChatModelIdentifier modelIdentifier,
+       [ActionParameter] GetTranslationIssuesXliffRequest input, [ActionParameter] GlossaryRequest glossary)
+    {
+        var systemPrompt = "Perform an LQA analysis and use the MQM error typology format using all 7 dimensions. " +
+                           "Here is a brief description of the seven high-level error type dimensions: " +
+                           "1. Terminology – errors arising when a term does not conform to normative domain or organizational terminology standards or when a term in the target text is not the correct, normative equivalent of the corresponding term in the source text. " +
+                           "2. Accuracy – errors occurring when the target text does not accurately correspond to the propositional content of the source text, introduced by distorting, omitting, or adding to the message. " +
+                           "3. Linguistic conventions  – errors related to the linguistic well-formedness of the text, including problems with grammaticality, spelling, punctuation, and mechanical correctness. " +
+                           "4. Style – errors occurring in a text that are grammatically acceptable but are inappropriate because they deviate from organizational style guides or exhibit inappropriate language style. " +
+                           "5. Locale conventions – errors occurring when the translation product violates locale-specific content or formatting requirements for data elements. " +
+                           "6. Audience appropriateness – errors arising from the use of content in the translation product that is invalid or inappropriate for the target locale or target audience. " +
+                           "7. Design and markup – errors related to the physical design or presentation of a translation product, including character, paragraph, and UI element formatting and markup, integration of text with graphical elements, and overall page or window layout. " +
+                           "Provide a quality rating for each dimension from 0 (completely bad) to 10 (perfect). You are an expert linguist and your task is to perform a Language Quality Assessment on input sentences. " +
+                           "Try to propose a fixed translation that would have no LQA errors. " +
+                           "Formatting: use line spacing between each category. The category name should be bold.";
+
+        if (glossary.Glossary != null)
+            systemPrompt +=
+                " Use the provided glossary entries for the respective languages. If there are discrepancies " +
+                "between the translation and glossary, note them in the 'Terminology' part of the report, " +
+                "along with terminology problems not related to the glossary.";
+
+        if (input.AdditionalPrompt != null)
+            systemPrompt = $"{systemPrompt} {input.AdditionalPrompt}";
+
+        var XLFservice = new XliffService(fileManagementClient);
+        var xliffDocument = await XLFservice.LoadXliffDocumentAsync(input.File);
+
+        var userPrompt =
+            $"{(input.SourceLanguage != null ? $"The {input.SourceLanguage} " : $"The {xliffDocument.SourceLanguage}: ")}\"{String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Source))}\" was translated as " +
+            $"\"{String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Target))}\"{(input.TargetLanguage != null ? $" into {input.TargetLanguage}" : $" into {xliffDocument.TargetLanguage}")}." +
+            $"{(input.TargetAudience != null ? $" The target audience is {input.TargetAudience}" : "")}";
+
+        if (glossary.Glossary != null)
+        {
+            var glossaryPromptPart = await GetGlossaryPromptPart(glossary.Glossary, String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Source)), true);
+            if (glossaryPromptPart != null) userPrompt += glossaryPromptPart;
+        }
+
+        var messages = new List<ChatMessageDto> { new(MessageRoles.System, systemPrompt), new(MessageRoles.User, userPrompt) };
+        var response = await ExecuteChatCompletion(messages, modelIdentifier.ModelId);
+
+        return new()
+        {
+            SystemPrompt = systemPrompt,
+            UserPrompt = userPrompt,
+            Message = response.Choices.First().Message.Content,
+            Usage = response.Usage,
+        };
+    }
+
+    [Action("Get MQM dimension values from XLIFF",
+        Description =
+            "Perform an LQA Analysis of a translated XLIFF file. The result will be in the MQM framework form. This action " +
+            "only returns the scores (between 1 and 10) of each dimension.")]
+    public async Task<MqmAnalysis> GetLqaDimensionValuesXLIFF([ActionParameter] TextChatModelIdentifier modelIdentifier,
+        [ActionParameter] GetTranslationIssuesXliffRequest input, [ActionParameter] GlossaryRequest glossary)
+    {
+        var systemPrompt = "Perform an LQA analysis and use the MQM error typology format using all 7 dimensions. " +
+                           "Here is a brief description of the seven high-level error type dimensions: " +
+                           "1. Terminology – errors arising when a term does not conform to normative domain or organizational terminology standards or when a term in the target text is not the correct, normative equivalent of the corresponding term in the source text. " +
+                           "2. Accuracy – errors occurring when the target text does not accurately correspond to the propositional content of the source text, introduced by distorting, omitting, or adding to the message. " +
+                           "3. Linguistic conventions  – errors related to the linguistic well-formedness of the text, including problems with grammaticality, spelling, punctuation, and mechanical correctness. " +
+                           "4. Style – errors occurring in a text that are grammatically acceptable but are inappropriate because they deviate from organizational style guides or exhibit inappropriate language style. " +
+                           "5. Locale conventions – errors occurring when the translation product violates locale-specific content or formatting requirements for data elements. " +
+                           "6. Audience appropriateness – errors arising from the use of content in the translation product that is invalid or inappropriate for the target locale or target audience. " +
+                           "7. Design and markup – errors related to the physical design or presentation of a translation product, including character, paragraph, and UI element formatting and markup, integration of text with graphical elements, and overall page or window layout. " +
+                           "Provide a quality rating for each dimension from 0 (completely bad) to 10 (perfect). You are an expert linguist and your task is to perform a Language Quality Assessment on input sentences. " +
+                           "Try to propose a fixed translation that would have no LQA errors. " +
+                           "The response should be in the following json format: " +
+                           "{\r\n  \"terminology\": 0,\r\n  \"accuracy\": 0,\r\n  \"linguistic_conventions\": 0,\r\n  \"style\": 0,\r\n  \"locale_conventions\": 0,\r\n  \"audience_appropriateness\": 0,\r\n  \"design_and_markup\": 0,\r\n  \"proposed_translation\": \"fixed translation\"\r\n}";
+
+        if (glossary.Glossary != null)
+            systemPrompt += " Use the provided glossary entries for the respective languages. If there are " +
+                            "discrepancies between the translation and glossary, reduce the score in the " +
+                            "'Terminology' part of the report respectively.";
+
+        if (input.AdditionalPrompt != null)
+            systemPrompt = $"{systemPrompt} {input.AdditionalPrompt}";
+
+        var XLFservice = new XliffService(fileManagementClient);
+        var xliffDocument = await XLFservice.LoadXliffDocumentAsync(input.File);
+
+        var userPrompt =
+            $"{(input.SourceLanguage != null ? $"The {input.SourceLanguage} " : $"The {xliffDocument.SourceLanguage}: ")}\"{String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Source))}\" was translated as " +
+            $"\"{String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Target))}\"{(input.TargetLanguage != null ? $" into {input.TargetLanguage}" : $" into {xliffDocument.TargetLanguage}")}." +
+            $"{(input.TargetAudience != null ? $" The target audience is {input.TargetAudience}" : "")}";
+
+        if (glossary.Glossary != null)
+        {
+            var glossaryPromptPart = await GetGlossaryPromptPart(glossary.Glossary, String.Join(" ", xliffDocument.TranslationUnits.Select(x => x.Source)), true);
+            if (glossaryPromptPart != null) userPrompt += glossaryPromptPart;
+        }
+
+        var messages = new List<ChatMessageDto> { new(MessageRoles.System, systemPrompt), new(MessageRoles.User, userPrompt) };
+        var response = await ExecuteChatCompletion(messages, modelIdentifier.ModelId, input, new { type = "json_object" });
+
+        try
+        {
+            var analysis = JsonConvert.DeserializeObject<MqmAnalysis>(response.Choices.First().Message.Content);
+            analysis.Usage = response.Usage;
+            return analysis;
+        }
+        catch
+        {
+            throw new Exception(
+                "Something went wrong parsing the output from OpenAI, most likely due to a hallucination!");
+        }
+    }
     #endregion
-    
+
     private async Task<(List<TranslationEntity>, UsageDto)> ProcessTranslationUnits(string prompt,
         XliffDocument xliff, string model,
         string systemPrompt, int bucketSize, FileReference? glossary, bool filter)
---
+++ Apps.OpenAI/Models/Requests/Xliff/GetTranslationIssuesXliffRequest.cs
@@ -0,0 +1,23 @@
+﻿using Apps.OpenAI.Models.Requests.Chat;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.OpenAI.Models.Requests.Xliff;
+public class GetTranslationIssuesXliffRequest : BaseChatRequest
+{
+    [Display("XLIFF file")]
+    public FileReference File { get; set; }
+
+    [Display("Additional prompt")]
+    public string? AdditionalPrompt { get; set; }
+
+    [Display("Source langauge")]
+    public string? SourceLanguage { get; set; }
+
+    [Display("Target langauge")]
+    public string? TargetLanguage { get; set; }
+
+    [Display("Target audience")]
+    public string? TargetAudience { get; set; }
+}
+
