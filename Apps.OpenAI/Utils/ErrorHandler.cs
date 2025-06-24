12a3781c8fdc926f02f92c058429db150218ff8c
RiabushenkoA <ariabushenko@blackbird.io>
2025-06-24 21:42:27 +0300

    Adde error handler wrapper to action 'Post-edit MT'

---
+++ Apps.OpenAI/Utils/ErrorHandler.cs
@@ -0,0 +1,45 @@
+﻿using Blackbird.Applications.Sdk.Common.Exceptions;
+using System;
+using System.Threading.Tasks;
+
+namespace Apps.OpenAI.Utils
+{
+    public static class ErrorHandler
+    {
+        public static async Task ExecuteWithErrorHandlingAsync(Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception ex)
+            {
+                throw new PluginApplicationException(ex.Message);
+            }
+        }
+
+        public static async Task<T> ExecuteWithErrorHandlingAsync<T>(Func<Task<T>> action)
+        {
+            try
+            {
+                return await action();
+            }
+            catch (Exception ex)
+            {
+                throw new PluginApplicationException(ex.Message);
+            }
+        }
+
+        public static T ExecuteWithErrorHandling<T>(Func<T> action)
+        {
+            try
+            {
+                return action();
+            }
+            catch (Exception ex)
+            {
+                throw new PluginApplicationException(ex.Message);
+            }
+        }
+    }
+}
