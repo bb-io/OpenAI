9b98892011b4324759e457fffb0c099ed98a1534
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-08-21 11:58:35 +0300

    fix(EditContentRequest): 🐛 Update `StaticDataSource` to use `OpenAiProcessFileFormatHandler` for output file handling

--- Apps.OpenAI/Models/Requests/Content/EditContentRequest.cs
+++ Apps.OpenAI/Models/Requests/Content/EditContentRequest.cs
@@ -1,7 +1,7 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.OpenAI.DataSourceHandlers.ModelDataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Files;
-using Blackbird.Applications.SDK.Blueprints.Handlers;
 using Blackbird.Applications.SDK.Blueprints.Interfaces.Edit;
 
 namespace Apps.OpenAI.Models.Requests.Content;
@@ -9,6 +9,6 @@ public class EditContentRequest : IEditFileInput
 {
     public FileReference File { get; set; }
 
-    [Display("Output file handling", Description = "Determine the format of the output file. The default Blackbird behavior is to convert to XLIFF for future steps."), StaticDataSource(typeof(ProcessFileFormatHandler))]
+    [Display("Output file handling", Description = "Determine the format of the output file. The default Blackbird behavior is to convert to XLIFF for future steps."), StaticDataSource(typeof(OpenAiProcessFileFormatHandler))]
     public string? OutputFileHandling { get; set; }
 }
