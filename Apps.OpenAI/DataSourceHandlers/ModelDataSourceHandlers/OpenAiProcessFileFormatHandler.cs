aebc54b11a848bd32c910e82de38f861678ea97d
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2025-08-21 11:52:32 +0300

    feat(translation): ✨ Add support for XLIFF 1.2 output format; Introduce `OpenAiProcessFileFormatHandler` for file format handling; Update `TranslateContentRequest` to utilize new handler

---
+++ Apps.OpenAI/DataSourceHandlers/ModelDataSourceHandlers/OpenAiProcessFileFormatHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.OpenAI.DataSourceHandlers.ModelDataSourceHandlers;
+
+public class OpenAiProcessFileFormatHandler : IStaticDataSourceItemHandler
+{
+    public IEnumerable<DataSourceItem> GetData()
+    {
+        return new List<DataSourceItem>()
+        {
+            new ("application/xliff+xml", "Interoperable XLIFF (default)"),
+            new("xliff1", "XLIFF 1.2"),
+            new("original", "Original data format")
+        };
+    }
+}
