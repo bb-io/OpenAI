3acde4610d35a94be2df081b5996b9d9bac1c0a1
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-29 18:01:14 +0200

    update RepurposeActions to use UniversalClient, update tests

--- Tests.OpenAI/RepurposeActionsTests.cs
+++ Tests.OpenAI/RepurposeActionsTests.cs
@@ -2,7 +2,6 @@
 using Apps.OpenAI.Actions;
 using Apps.OpenAI.Models.Identifiers;
 using Apps.OpenAI.Models.Requests.Chat;
-using Blackbird.Applications.Sdk.Common.Exceptions;
 
 namespace Tests.OpenAI;
 
@@ -10,22 +9,22 @@ namespace Tests.OpenAI;
 public class RepurposeActionsTests : TestBase
 {
     [TestMethod]
-    public async Task CreateSummary_EmptyRequiredInputs_ThrowsException()
+    public async Task CreateSummary_ReturnsRepurposeResponse()
     {
         foreach (var context in InvocationContext)
         {
 		    // Arrange
 		    var actions = new RepurposeActions(context, FileManagementClient);
-		    string emptyContent = "";
-            var emptyModelIdentifier = new TextChatModelIdentifier { ModelId = "" };
+		    var content = "Hello world! This needs to be repurposed";
+            var model = new TextChatModelIdentifier { ModelId = "gpt-4.1" };
+            var request = new RepurposeRequest { ToneOfVOice = "very angry" };
 
             // Act
-            var ex = await Assert.ThrowsExceptionAsync<PluginMisconfigurationException>(
-                async () => await actions.CreateSummary(emptyModelIdentifier, emptyContent, new RepurposeRequest { }, new GlossaryRequest { })
-            );
+            var result = await actions.CreateSummary(model, content, request, new GlossaryRequest { });
 
             // Assert
-            Equals(ex.Message, "These parameters are required and can't be empty: Model; Text; ");
+            PrintResult(context, result);
+            Assert.IsNotNull(result);
         }
     }
 }
