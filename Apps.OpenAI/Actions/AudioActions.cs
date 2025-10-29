f6b1782c1aa7bf59267ab936ba9e491d166b8bb6
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-29 11:02:13 +0200

    update audio actions

--- Apps.OpenAI/Actions/AudioActions.cs
+++ Apps.OpenAI/Actions/AudioActions.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Apps.OpenAI.Actions.Base;
+﻿using Apps.OpenAI.Actions.Base;
+using Apps.OpenAI.Api.Requests;
+using Apps.OpenAI.Constants;
 using Apps.OpenAI.Dtos;
 using Apps.OpenAI.Models.Identifiers;
 using Apps.OpenAI.Models.Requests;
@@ -10,12 +8,16 @@ using Apps.OpenAI.Models.Requests.Audio;
 using Apps.OpenAI.Models.Responses.Audio;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
-using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Blackbird.Applications.Sdk.Utils.Extensions.Files;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Newtonsoft.Json;
 using RestSharp;
-using Apps.OpenAI.Api.Requests;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Apps.OpenAI.Actions;
 
@@ -27,6 +29,7 @@ public class AudioActions(InvocationContext invocationContext, IFileManagementCl
                                                         "video file (mp3, mp4, mpeg, mpga, m4a, wav, or webm).")]
     public async Task<TranslationResponse> CreateTranslation([ActionParameter] TranslationRequest input)
     {
+        ThrowForAzure();
         var request = new OpenAIRequest("/audio/translations", Method.Post);
         var fileStream = await FileManagementClient.DownloadAsync(input.File);
         var fileBytes = await fileStream.GetByteData();
@@ -35,17 +38,15 @@ public class AudioActions(InvocationContext invocationContext, IFileManagementCl
         request.AddParameter("response_format", "verbose_json");
         request.AddParameter("temperature", input.Temperature ?? 0);
 
-        var response = await Client.ExecuteWithErrorHandling<TextDto>(request);
-        return new()
-        {
-            TranslatedText = response.Text
-        };
+        var response = await UniversalClient.ExecuteWithErrorHandling<TextDto>(request);
+        return new() { TranslatedText = response.Text };
     }
 
     [Action("Create transcription", Description = "Generates a transcription given an audio or video file (mp3, " +
                                                   "mp4, mpeg, mpga, m4a, wav, or webm).")]
     public async Task<TranscriptionResponse> CreateTranscription([ActionParameter] TranscriptionRequest input)
     {
+        ThrowForAzure();
         var request = new OpenAIRequest("/audio/transcriptions", Method.Post);
         var fileStream = await FileManagementClient.DownloadAsync(input.File);
         var fileBytes = await fileStream.GetByteData();
@@ -63,7 +64,7 @@ public class AudioActions(InvocationContext invocationContext, IFileManagementCl
             }
         }
         
-        var response = await Client.ExecuteWithErrorHandling<TranscriptionDto>(request);
+        var response = await UniversalClient.ExecuteWithErrorHandling<TranscriptionDto>(request);
         var words = response.Words?.Select(x => new WordResponse(x)).ToList() ?? new List<WordResponse>();
         var segments = response.Segments?.Select(x => new SegmentResponse(x)).ToList() ?? new List<SegmentResponse>();
         
@@ -80,6 +81,7 @@ public class AudioActions(InvocationContext invocationContext, IFileManagementCl
         [ActionParameter] SpeechCreationModelIdentifier modelIdentifier,
         [ActionParameter] CreateSpeechRequest input)
     {
+        ThrowForAzure();
         var model = modelIdentifier.ModelId ?? "tts-1-hd";
         var responseFormat = input.ResponseFormat ?? "mp3";
 
@@ -93,12 +95,18 @@ public class AudioActions(InvocationContext invocationContext, IFileManagementCl
             speed = input.Speed ?? 1.0f
         });
 
-        var response = await Client.ExecuteWithErrorHandling(request);
+        var response = await UniversalClient.ExecuteWithErrorHandling(request);
 
         using var stream = new MemoryStream(response.RawBytes);
         var file = await FileManagementClient.UploadAsync(stream, response.ContentType,
             $"{input.OutputAudioName ?? input.Voice}.{responseFormat}");
 
         return new(file);
+    }
+
+    private void ThrowForAzure()
+    {
+        if (UniversalClient.ConnectionType == ConnectionTypes.AzureOpenAi)
+            throw new PluginMisconfigurationException("Azure OpenAI does not support audio actions. Please use OpenAI for such tasks");
     }
 }
