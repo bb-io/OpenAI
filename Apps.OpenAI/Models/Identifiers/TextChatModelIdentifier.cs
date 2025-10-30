8ba1778b0fc40f4061cb657ba1d0dbfbe1a26ea8
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-30 12:52:30 +0200

    add description to TextChatModelIdentifier.ModelId, update csproj

--- Apps.OpenAI/Models/Identifiers/TextChatModelIdentifier.cs
+++ Apps.OpenAI/Models/Identifiers/TextChatModelIdentifier.cs
@@ -6,6 +6,10 @@ namespace Apps.OpenAI.Models.Identifiers;
 
 public class TextChatModelIdentifier
 {
-    [Display("Model"), DataSource(typeof(TextChatModelDataSourceHandler))]
+    [Display("Model", Description =
+        "Required for standard OpenAI connection type. " +
+        "Ovewrites the model from the connection for OpenAI (embedded). " +
+        "Does not work for Azure OpenAI connection type (specify the deployment name during connection)")] 
+    [DataSource(typeof(TextChatModelDataSourceHandler))]
     public string? ModelId { get; set; }
 }
