11837929b5332edea01073c3d63b49e550432e81
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-30 11:02:03 +0200

    remove OpenAiClient, unused RunRequest and AssistantsDataSourceHandler (and delete its test)

--- Tests.OpenAI/DataSourcesTests.cs
+++ Tests.OpenAI/DataSourcesTests.cs
@@ -50,19 +50,6 @@ public class DataSourceHandlerTests : TestBase
     }
 
     [TestMethod]
-    public async Task GetDataAsync_ForAssistantModels_ReturnsNonEmptyCollection()
-    {
-        foreach (var context in InvocationContext)
-        {
-            var handler = new AssistantsDataSourceHandler(context);
-            var data = await handler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            PrintResult(data, context);
-            Assert.AreNotEqual(data.Count(), 0);
-        }
-    }
-
-    [TestMethod]
     public async Task GetDataAsync_ForBatches_ReturnsNonEmptyCollection()
     {
         foreach (var context in InvocationContext)
