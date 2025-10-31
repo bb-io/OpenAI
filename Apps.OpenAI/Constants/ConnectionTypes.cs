2d4ecd616aeb6230ff5b19f8f421c807a8642a60
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-31 10:14:29 +0200

    add comment to ConnectionTypes

--- Apps.OpenAI/Constants/ConnectionTypes.cs
+++ Apps.OpenAI/Constants/ConnectionTypes.cs
@@ -4,7 +4,7 @@ namespace Apps.OpenAI.Constants;
 
 public static class ConnectionTypes
 {
-    public const string OpenAi = "Developer API token";
+    public const string OpenAi = "Developer API token";     // Legacy OpenAI connection identifier. Do not change!
     public const string AzureOpenAi = "AzureConnection";
     public const string OpenAiEmbedded = "OpenAiConnectionEmbedded";
 
