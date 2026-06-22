e64f789272b8b2cc1316e821fc408fd9e7b2b247
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-06-22 16:52:00 +0300

    remove even more dead code

--- Apps.OpenAI/Constants/KnownErrorMessages.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Apps.OpenAI.Constants;
-
-public static class KnownErrorMessages
-{
-    public const string CannotConvertToContent = "Cannot convert to content, no original data found";
-    public const string CouldNotDetectContentType = "Could not detect any valid content type this library can process";
-}
